8ed5bbac937839550ff4b02098a25d69e15ac16f
Evan Bishop <evanpbishop@gmail.com>
2021-04-21 23:29:05 -0400

    Added Suited Llamium also Initial setup

---
+++ src/main/java/me/hyperboid/testmod/ExampleMod.java
@@ -0,0 +1,21 @@
+package me.hyperboid.testmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "hyperstest";
+	public static final Item TEST_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "test_item"), TEST_ITEM);
+	}
+}
--- src/main/java/me/hyperboid/testmod/mixin/ExampleMixin.java
+++ src/main/java/me/hyperboid/testmod/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package me.hyperboid.testmod.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
