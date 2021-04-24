45cd39c30bde75740efb8fc25a540457198beb91
Evan Bishop <evanpbishop@gmail.com>
2021-04-23 23:27:51 -0400

    Obamium now generates as an ore

--- src/main/java/me/hyperboid/testmod/ExampleMod.java
+++ src/main/java/me/hyperboid/testmod/ExampleMod.java
@@ -1,6 +1,8 @@
 package me.hyperboid.testmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
@@ -10,7 +12,15 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = "hyperstest";
@@ -32,6 +42,14 @@ public class ExampleMod implements ModInitializer {
 		.sounds(BlockSoundGroup.STONE)
 		.breakByTool(FabricToolTags.PICKAXES)
 	);
+
+	private static ConfiguredFeature<?,?> OBAMIUM_ORE_OVERWORLD = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, OBAMIUM_BLOCK.getDefaultState(), 4))
+	// TODO: Change to -64 for 1.17
+	.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 64)))
+	.spreadHorizontally()
+	.repeat(60)
+	;
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -68,5 +86,8 @@ public class ExampleMod implements ModInitializer {
 				.group(ItemGroup.BUILDING_BLOCKS)
 			)
 		);
+		RegistryKey<ConfiguredFeature<?,?>> obamiumOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(MOD_ID, "obamium_block"));
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, obamiumOreOverworld.getValue(), OBAMIUM_ORE_OVERWORLD);
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, obamiumOreOverworld);
 	}
 }
